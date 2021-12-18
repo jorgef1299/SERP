266f5e375913e373ab6dbc6c71846a927167ab73
JoaoSilvaFeup <61052017+JoaoSilvaFeup@users.noreply.github.com>
2021-12-18 16:33:25 +0000

    Code simplified

--- serp/include/rpi_cam.h
+++ serp/include/rpi_cam.h
@@ -3,9 +3,14 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 #include <sensor_msgs/Image.h>
-
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 #include <iostream>
+
+
+// GLOBAL VARIABLES
+
+// Defining the dimensions of checkerboard
+int CHECKERBOARD[2]{9,6};
