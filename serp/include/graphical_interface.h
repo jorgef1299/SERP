82ddb816b9798919aa4001a7fa7f77e041d04ae2
Jorge Ferreira <jorgef1299@gmail.com>
2021-12-20 00:33:35 +0000

    Interface desenhada pela Ana + Ranges working

--- serp/include/graphical_interface.h
+++ serp/include/graphical_interface.h
@@ -11,8 +11,26 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
 
 // Global Variables
+GtkStyleContext *context;
 GtkTextView *log_mensagens;
 GtkTextBuffer *log_buffer;
 GtkTextIter *log_text_iter;
+GtkWidget *button_manual_go;
+GtkWidget *button_manual_stop;
+GtkRange *range_motor_left;
+GtkRange *range_motor_right;
 ros::ServiceClient client_velocity_setpoint;
 ros::Publisher pub_twist;
+
+enum RobotState {
+    Stopped,
+    ManualControl
+};
+
+struct Robot {
+    int8_t motor_left_velocity;
+    int8_t motor_right_velocity;
+    int8_t motor_left_requested_velocity;
+    int8_t motor_right_requested_velocity;
+    RobotState state;
+} robot;
--- serp/src/graphical_interface.cpp
+++ serp/src/graphical_interface.cpp
@@ -18,21 +18,48 @@ std::string stampToString(const ros::Time& stamp, const std::string format="%Y-%
     std::time_t raw_time = static_cast<time_t>(stamp.sec);
     struct tm* timeinfo = localtime(&raw_time);
     std::strftime(output, output_size, format.c_str(), timeinfo);
-    return std::string(output);
+    std::stringstream ss;
+    ss << std::setw(9) << std::setfill('0') << stamp.nsec;
+    const size_t fractional_second_digits = 3;
+    return std::string(output) + "." + ss.str().substr(0, fractional_second_digits);
 }
 
-void insert_text_to_log(GtkTextView* text_view, GtkTextBuffer* text_buffer, GtkTextIter* text_iter, const char* text, std::string mode) {
+void insert_text_to_log(GtkTextView* text_view, GtkTextBuffer* text_buffer, GtkTextIter* text_iter, const char* text, bool erro=false) {
     gtk_text_buffer_get_end_iter(text_buffer, text_iter);
     std::string str_text(text);
     std::string str_actual_time;
     std::string str_text_to_insert;
     std::string color;
     str_actual_time = stampToString(ros::Time::now());
-    if(mode == "normal") color = "black";
-    else if(mode == "error") color = "red";
+    if(!erro) color = "black";
+    else color = "red";
     str_text_to_insert = "<span color=\"" + color + "\">> [" + str_actual_time + "] - " + str_text + "</span>\n";
     gtk_text_buffer_insert_markup(text_buffer, text_iter, str_text_to_insert.c_str(), -1);
     gtk_text_view_scroll_to_mark(text_view, gtk_text_buffer_get_insert(text_buffer), 0,true,1,1);
+}
+
+void enable_interface_button(GtkWidget *button, bool is_green=true) {
+    context = gtk_widget_get_style_context(button);
+    if(is_green) {
+        gtk_style_context_add_class(context, "button_green_active");
+    }
+    else {
+        gtk_style_context_add_class(context, "button_red_active");
+    }
+}
+
+void disable_interface_button(GtkWidget *button, bool is_green=true) {
+    context = gtk_widget_get_style_context(button);
+    if(is_green) {
+        gtk_style_context_remove_class(context, "button_green_active");
+    }
+    else {
+        gtk_style_context_remove_class(context, "button_red_active");
+    }
+}
+
+void initializeGtkInterface() {
+    robot.state = Stopped;
 }
 
 int main(int argc, char *argv[])
@@ -66,6 +93,10 @@ int main(int argc, char *argv[])
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
     log_mensagens = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "log_text_view"));
     log_buffer = GTK_TEXT_BUFFER(gtk_builder_get_object(builder, "log_buffer"));
+    button_manual_go = GTK_WIDGET(gtk_builder_get_object(builder, "button_manual_go"));
+    button_manual_stop = GTK_WIDGET(gtk_builder_get_object(builder, "button_manual_stop"));
+    range_motor_left = GTK_RANGE(gtk_builder_get_object(builder, "range_motor_left"));
+    range_motor_right = GTK_RANGE(gtk_builder_get_object(builder, "range_motor_right"));
 
     // Create text buffer iterator
     log_text_iter = new GtkTextIter();
@@ -78,6 +109,9 @@ int main(int argc, char *argv[])
     // GTK connect signals
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_builder_connect_signals(builder, NULL);
+
+    // Initialize variables
+    initializeGtkInterface();
 
     // Init ROS node
     ros::init(argc, argv, "serp_interface_node");
@@ -99,30 +133,98 @@ int main(int argc, char *argv[])
 extern "C"
 {
     void on_button_go_manual_control_clicked(GtkButton *clicked_button) {
-        ROS_INFO("Botão GO clicado!"); //TODO: DELETE
-        // TODO: Check if the velocity values are valid, if not -> clear the values
+        if(robot.state == Stopped) {
+            // Send service request
+            serp::VelocitySetPoint srv;
+            srv.request.state = true;
+            srv.request.vel_motor_left = robot.motor_left_requested_velocity;
+            srv.request.vel_motor_right = robot.motor_right_requested_velocity;
+            if(client_velocity_setpoint.call(srv) && srv.response.success) {
+                // Change buttons color
+                enable_interface_button(button_manual_go, true);
+                disable_interface_button(button_manual_stop, false);
+                robot.state = ManualControl;
+                // Add text to the log box
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Controlo manual ativado!");
+            }
+            else {
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro: Não foi possível ativar o modo de controlo manual...", true);
+            }
 
-        // Send service request //TODO: Send the inserted values instead of the following ones
-        serp::VelocitySetPoint srv;
-        srv.request.state = true; // activate setpoint mode
-        srv.request.vel_motor_left = 42;
-        srv.request.vel_motor_right = 23;
-        if(client_velocity_setpoint.call(srv) && srv.response.success) {
-            ROS_INFO("Successfully defined velocity setpoint to: Motor_Left=%d%%\tMotor_Right=%d%%", srv.request.vel_motor_left, srv.request.vel_motor_right);
         }
-        insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Mensagem de teste", "normal");
     }
 
     void on_button_stop_manual_control_clicked(GtkButton *clicked_button) {
-        // Send service request
-        serp::VelocitySetPoint srv;
-        srv.request.state = false;
-        if(client_velocity_setpoint.call(srv) && srv.response.success) {
-            ROS_INFO("Successfully stopped velocity setpoint!");
+        if(robot.state == ManualControl) {
+            // Send service request
+            serp::VelocitySetPoint srv;
+            srv.request.state = false;
+            srv.request.vel_motor_left = robot.motor_left_velocity;
+            srv.request.vel_motor_right = robot.motor_right_velocity;
+            if(client_velocity_setpoint.call(srv) && srv.response.success) {
+                // Change button color to appear disabled
+                disable_interface_button(button_manual_go, true);
+                enable_interface_button(button_manual_stop, false);
+                robot.state = Stopped;
+                // Reset Ranges
+                gtk_range_set_value(range_motor_left, 0);
+                gtk_range_set_value(range_motor_right, 0);
+                // Add text to the log box
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Controlo manual desativado!");
+            }
+            else {
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro: Não foi possível desativar o modo de controlo manual...", true);
+            }
+        }
+    }
+
+    void on_range_left_motor_changed(GtkRange *range) {
+        gdouble value;
+        value = gtk_range_get_value(range);
+        if (robot.state == ManualControl) {
+            // Send service request
+            serp::VelocitySetPoint srv;
+            srv.request.state = true; // activate setpoint mode
+            srv.request.vel_motor_left = (int8_t) value;
+            srv.request.vel_motor_right = robot.motor_right_requested_velocity; // Only update motor left velocity
+            if (!(client_velocity_setpoint.call(srv) && srv.response.success)) {
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro: Não foi possível atualizar manualmente a velocidade do robô.", true);
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro: Controlo manual desativado, por segurança!", true);
+                robot.state = Stopped;
+                enable_interface_button(button_manual_stop, false);
+                disable_interface_button(button_manual_go, true);
+            }
+            else {
+                robot.motor_left_requested_velocity = srv.request.vel_motor_left;
+            }
         }
         else {
-            ROS_ERROR("Couldn't stop velocity setpoint...");
+            robot.motor_left_requested_velocity = (int8_t) value;
         }
-        insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro...", "error");
+    }
+
+    void on_range_right_motor_changed(GtkRange *range) {
+        gdouble value;
+        value = gtk_range_get_value(range);
+        if (robot.state == ManualControl) {
+            // Send service request
+            serp::VelocitySetPoint srv;
+            srv.request.state = true; // activate setpoint mode
+            srv.request.vel_motor_left = robot.motor_left_requested_velocity; // Only update motor right velocity
+            srv.request.vel_motor_right = (int8_t) value;
+            if (!(client_velocity_setpoint.call(srv) && srv.response.success)) {
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro: Não foi possível atualizar manualmente a velocidade do robô.", true);
+                insert_text_to_log(log_mensagens, log_buffer, log_text_iter, "Erro: Controlo manual desativado, por segurança!", true);
+                robot.state = Stopped;
+                enable_interface_button(button_manual_stop, false);
+                disable_interface_button(button_manual_go, true);
+            }
+            else {
+                robot.motor_right_requested_velocity = srv.request.vel_motor_right;
+            }
+        }
+        else {
+            robot.motor_right_requested_velocity = (int8_t) value;
+        }
     }
 }
