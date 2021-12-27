588ad34e6218753b2ca9a1c9856f076c31579888
JoaoSilvaFeup <61052017+JoaoSilvaFeup@users.noreply.github.com>
2021-12-27 10:18:32 +0000

    video opening

--- serp/include/video_tests.h
+++ serp/include/video_tests.h
@@ -3,6 +3,7 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <iostream>
+//#include <opencv2/videoio.hpp>
 
 // export SERP_PROJECT_PATH=~/catkin_ws/src/SERP/serp/
 #include "calibration.h"
--- serp/src/video_tests.cpp
+++ serp/src/video_tests.cpp
@@ -6,23 +6,9 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "video_tests_node");
     ros::NodeHandle n_public;
 
-    // Extracting path of individual image stored in a given directory
-    std::vector<cv::String> images;
-
-    // Path of the folder containing checkerboard images
-    // export SERP_PROJECT_PATH=~/catkin_ws/src/SERP/serp/
-    char *project_path = getenv("SERP_PROJECT_PATH");
-    char img_file_name[25] = "include/tests/*.jpg";
-    char img_file_path[200];
-    sprintf(img_file_path, "%s%s", project_path, img_file_name);
-
-    cv::glob(img_file_path, images);
-
-
     // Create a VideoCapture object and open the input file
       // If the input is the web camera, pass 0 instead of the video file name
-      cv::VideoCapture cap(images[7]);
-//      cv::VideoCapture cap("../include/tests/random_video.h264");
+      cv::VideoCapture cap("../catkin_ws/src/SERP/serp/include/tests/random_video.h264");
 
       // Check if camera opened successfully
       if(!cap.isOpened())
