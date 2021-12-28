c50a264e9508bc2afff9c8c85ca0369654f69774
Jorge Ferreira <jorgef1299@gmail.com>
2021-12-28 11:15:53 +0000

    Fix missing msgs

---
+++ serp/launch/GlobalLauncher/launch_serp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Update application icon
+mv serp.desktop serp.desktop-bak
+sed -e "s,Icon=.*,Icon=$PWD/icon.png,g" serp.desktop-bak > serp.desktop
+rm serp.desktop-bak
+
+export SERP_PROJECT_PATH=~/catkin_ws/src/serp/
+. ~/catkin_ws/devel/setup.bash
+roscore &
+rosrun serp graphical_interface
